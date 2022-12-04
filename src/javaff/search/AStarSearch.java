8c8d8ad1681be13b100b0fc003a1f6b810d540e7
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2022-12-03 22:30:02 -0300

    fix: changin the way the sum is made on HGValueComparator, this fixes a lot of issues

--- src/javaff/search/AStarSearch.java
+++ src/javaff/search/AStarSearch.java
@@ -7,68 +7,63 @@ import java.util.TreeSet;
 import java.util.Hashtable;
 
 public class AStarSearch extends Search {
-    protected Hashtable closed;
-	protected TreeSet open;
-	protected Filter filter = null;
-	
-	public AStarSearch(State s)
-    {
-		this(s, new HGValueComparator());
-	}
+  protected Hashtable closed;
+  protected TreeSet open;
+  protected Filter filter = null;
 
-	public AStarSearch(State s, Comparator c)
-    {
-		super(s);
-		setComparator(c);
-		
-		closed = new Hashtable();
-		open = new TreeSet(comp);
-	}
+  public AStarSearch(State s) {
+    this(s, new HGValueComparator());
+  }
 
-	public void setFilter(Filter f)
-	{
-		filter = f;
-	}
+  public AStarSearch(State s, Comparator c) {
+    super(s);
+    setComparator(c);
 
-	public void updateOpen(State S)
-    {
-		open.addAll(S.getNextStates(filter.getActions(S)));
-	}
+    closed = new Hashtable();
+    open = new TreeSet(comp);
+  }
 
-	public State removeNext()
-    {
-		State S = (State) ((TreeSet) open).first();
-		open.remove(S);
- 		return S;
-	}
+  public void setFilter(Filter f) {
+    filter = f;
+  }
 
-	public boolean needToVisit(State s) {
-		Integer Shash = Integer.valueOf(s.hashCode());
-		State D = (State) closed.get(Shash);
-		
-		if (closed.containsKey(Shash) && D.equals(s)) return false;
-		
-		closed.put(Shash, s);
-		return true;
-	}
+  public void updateOpen(State S) {
+    open.addAll(S.getNextStates(filter.getActions(S)));
+  }
 
-	public State search() {
-		
-		open.add(start);
+  public State removeNext() {
+    State S = (State) ((TreeSet) open).first();
+    open.remove(S);
+    return S;
+  }
 
-		while (!open.isEmpty())
-		{
-			State s = removeNext();
-			if (needToVisit(s)) {
-				++nodeCount;
-				if (s.goalReached()) {
-					return s;
-				} else {
-					updateOpen(s);
-				}
-			}
-			
-		}
-		return null;
-	}
+  public boolean needToVisit(State s) {
+    Integer Shash = Integer.valueOf(s.hashCode());
+    State D = (State) closed.get(Shash);
+
+    if (closed.containsKey(Shash) && D.equals(s))
+      return false;
+
+    closed.put(Shash, s);
+    return true;
+  }
+
+  public State search() {
+
+    open.add(start);
+
+    while (!open.isEmpty()) {
+      State s = removeNext();
+      if (needToVisit(s)) {
+        ++nodeCount;
+        if (s.goalReached()) {
+          return s;
+        } else {
+          updateOpen(s);
+        }
+      }
+
+    }
+    return null;
+  }
 }
