e21a7315af9b28a2e8b26515f25d68e416633846
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2022-12-03 22:33:57 -0300

    Revert "fix: changin the way the sum is made on HGValueComparator, this fixes a lot of issues"
    This reverts commit 8c8d8ad1681be13b100b0fc003a1f6b810d540e7.

--- src/javaff/search/AStarSearch.java
+++ src/javaff/search/AStarSearch.java
@@ -7,63 +7,68 @@ import java.util.TreeSet;
 import java.util.Hashtable;
 
 public class AStarSearch extends Search {
-  protected Hashtable closed;
-  protected TreeSet open;
-  protected Filter filter = null;
+    protected Hashtable closed;
+	protected TreeSet open;
+	protected Filter filter = null;
+	
+	public AStarSearch(State s)
+    {
+		this(s, new HGValueComparator());
+	}
 
-  public AStarSearch(State s) {
-    this(s, new HGValueComparator());
-  }
+	public AStarSearch(State s, Comparator c)
+    {
+		super(s);
+		setComparator(c);
+		
+		closed = new Hashtable();
+		open = new TreeSet(comp);
+	}
 
-  public AStarSearch(State s, Comparator c) {
-    super(s);
-    setComparator(c);
+	public void setFilter(Filter f)
+	{
+		filter = f;
+	}
 
-    closed = new Hashtable();
-    open = new TreeSet(comp);
-  }
+	public void updateOpen(State S)
+    {
+		open.addAll(S.getNextStates(filter.getActions(S)));
+	}
 
-  public void setFilter(Filter f) {
-    filter = f;
-  }
+	public State removeNext()
+    {
+		State S = (State) ((TreeSet) open).first();
+		open.remove(S);
+ 		return S;
+	}
 
-  public void updateOpen(State S) {
-    open.addAll(S.getNextStates(filter.getActions(S)));
-  }
+	public boolean needToVisit(State s) {
+		Integer Shash = Integer.valueOf(s.hashCode());
+		State D = (State) closed.get(Shash);
+		
+		if (closed.containsKey(Shash) && D.equals(s)) return false;
+		
+		closed.put(Shash, s);
+		return true;
+	}
 
-  public State removeNext() {
-    State S = (State) ((TreeSet) open).first();
-    open.remove(S);
-    return S;
-  }
+	public State search() {
+		
+		open.add(start);
 
-  public boolean needToVisit(State s) {
-    Integer Shash = Integer.valueOf(s.hashCode());
-    State D = (State) closed.get(Shash);
-
-    if (closed.containsKey(Shash) && D.equals(s))
-      return false;
-
-    closed.put(Shash, s);
-    return true;
-  }
-
-  public State search() {
-
-    open.add(start);
-
-    while (!open.isEmpty()) {
-      State s = removeNext();
-      if (needToVisit(s)) {
-        ++nodeCount;
-        if (s.goalReached()) {
-          return s;
-        } else {
-          updateOpen(s);
-        }
-      }
-
-    }
-    return null;
-  }
+		while (!open.isEmpty())
+		{
+			State s = removeNext();
+			if (needToVisit(s)) {
+				++nodeCount;
+				if (s.goalReached()) {
+					return s;
+				} else {
+					updateOpen(s);
+				}
+			}
+			
+		}
+		return null;
+	}
 }
--- src/javaff/search/HGValueComparator.java
+++ src/javaff/search/HGValueComparator.java
@@ -4,27 +4,26 @@ import javaff.planning.State;
 import java.util.Comparator;
 import java.math.BigDecimal;
 
-public class HGValueComparator implements Comparator {
-  public int compare(Object obj1, Object obj2) {
-    int r = 0;
-    if ((obj1 instanceof State) && (obj2 instanceof State)) {
-      State s1 = (State) obj1;
-      State s2 = (State) obj2;
-      BigDecimal d1 = s1.getHValue();
-      BigDecimal d2 = s2.getHValue();
-      d1.add(s1.getGValue());
-      d2.add(s2.getGValue());
-      r = d1.compareTo(d2);
-      if (r == 0) {
-        if (s1.hashCode() > s2.hashCode())
-          r = 1;
-        else if (s1.hashCode() == s2.hashCode() && s1.equals(s2))
-          r = 0;
-        else
-          r = -1;
-      }
-    }
-    return r;
-  }
+public class HGValueComparator implements Comparator
+{
+	public int compare(Object obj1, Object obj2)
+    {
+		int r = 0;
+		if ((obj1 instanceof State) && (obj2 instanceof State))
+		{
+			State s1 = (State) obj1;
+			State s2 = (State) obj2;
+			BigDecimal d1 = s1.getHValue().add(s1.getGValue());
+			BigDecimal d2 = s2.getHValue().add(s2.getGValue());
+			r = d1.compareTo(d2);
+			if (r == 0)
+			{
+				if (s1.hashCode() > s2.hashCode()) r = 1;
+				else if (s1.hashCode() == s2.hashCode() && s1.equals(s2)) r=0;
+				else r = -1;
+			}
+		}
+		return r;
+	}
 
-}
+} 
